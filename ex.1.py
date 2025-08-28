06d19d8dee0a425eaec2d4db9cb474a187d8e51f
karth <karhikdheeravath718@gmail.com>
2025-08-28 08:51:34 +0530

    added

--- ex.1.py
+++ ex.1.py
@@ -2,3 +2,9 @@ print("Good Morning")
 print("My Name Is Karthik Iam From Mlg")
 print("This Is Karthik Branch")
 print("my name is karthik today data is 28th")
+
+
+a = 20
+b = 30
+c = a + b
+print(c)
